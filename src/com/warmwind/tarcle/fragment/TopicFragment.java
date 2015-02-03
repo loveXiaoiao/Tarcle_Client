e9fe9606243c9aeb7590da30da7617328d1bc119
loveXiaoiao <liyi2010112228@163.com>
2015-02-03 17:03:42 +0800

    update views

--- src/com/warmwind/tarcle/fragment/TopicFragment.java
+++ src/com/warmwind/tarcle/fragment/TopicFragment.java
@@ -1,27 +1,16 @@
 package com.warmwind.tarcle.fragment;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import com.warmwind.tarcle.R;
-import com.warmwind.tarcle.view.MailListView;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 public class TopicFragment extends Fragment {
 	
-	private View mParent;
-	private ListView listView;
-	private FragmentActivity mActivity;
-	List<String> contacts = new ArrayList<String>();
-	private MailListView mailListView;
-	String a[] = {"新的朋友","群聊","标签","公众号","啊亲爱的","白河静流","彪哥","CC","程果","陈辉NewYork","戴乐"};
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,18 +19,5 @@ public class TopicFragment extends Fragment {
 		return view;
 	}
 	
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-		mParent = getView();
-		mActivity = getActivity();
-		for(int i=0;i<a.length;i++){
-			contacts.add(a[i]);
-		}
-		mailListView = new MailListView(mActivity, contacts);
-		listView = (ListView) mParent.findViewById(R.id.MailListItem);
-		listView.setAdapter(mailListView);
-		
-	}
 
 }
